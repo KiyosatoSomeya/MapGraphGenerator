942113568ae8c2da515975fe7d807ed99ba6a386
KiyosatoSomeya <kiyo_8into32@me.com>
2019-09-26 01:37:38 +0900

    緯度経度高度の入力機能の追加

---
+++ js/graphGenerator.js
@@ -0,0 +1,46 @@
+var map;
+var elevationObj;
+
+class map_node{
+  constructor(latitude, longitude, elevation, info){
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.elevation = elevation;
+    this.info = info;
+  }
+}
+
+class map_edge{
+  constructor(node_1, node_2, info){
+    this.node_1 = node_1;
+    this.node_2 = node_2;
+    this.info = info;
+  }
+}
+
+function initMap() {
+    var target = document.getElementById('map');
+
+    map = new google.maps.Map(target, {
+    center: {lat: 35.605232, lng: 139.683530},
+    zoom: 14
+    });
+
+    elevationObj = new google.maps.ElevationService();
+}
+
+function SetLatLngElev(){
+  var latlng = map.getCenter();
+  document.getElementById("latInput") = latlng.lat();
+  document.getElementById("lngInput") = latlng.lng();
+
+  var request = {locations: new Array(latlng)}
+  elevationObj.getElevationForLocations(request, function(response, status){
+    if(status == google.maps.ElevationStatus.OK){
+      document.getElementById("elevInput") = response[0].elevation;
+    }else{
+      alert("Could not get elevation");
+    }
+  })
+}
+
--- js/initMap.js
+++
@@ -1,9 +0,0 @@
-var map;
-function initMap() {
-    var target = document.getElementById('map');
-
-    map = new google.maps.Map(target, {
-    center: {lat: 35.605232, lng: 139.683530},
-    zoom: 14
-    });
-}
